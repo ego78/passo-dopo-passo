bec4fca5e62c80d860c7ef8d576f4e5a99da023b
ego78 <ego.giovanni@gmail.com>
2026-07-28 11:18:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Incolla tra le virgolette l'URL della distribuzione Apps Script che termina con /exec
 window.APP_CONFIG = {
-  apiUrl: "INCOLLA_QUI_URL_APPS_SCRIPT_EXEC"
+  apiUrl: "https://script.google.com/macros/s/AKfycbxc87F8szzXHQnYcuiukMQczWPG32uBuC-GmwWBFPMWcrZkCT3zKcSYACOL0otDjzqz_Q/exec"
 };
